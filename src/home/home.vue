c2d3822e861345712d603d7eb8f512cd7a908157
TNTSuperMan <tntsuperga@gmail.com>
2024-05-29 19:14:35 +0900

    q

---
+++ src/home/home.vue
@@ -0,0 +1,9 @@
+<script setup>
+import {RouterLink} from "vue-router";
+</script>
+<template>
+    <h1>HTOffice</h1>
+    <RouterLink to="/docs" class="applinks">HTOffice Docs</RouterLink>
+    <RouterLink to="/" class="applinks">HTOffice Table</RouterLink>
+    <RouterLink to="/" class="applinks">HTOffice Emphasis</RouterLink>
+</template>
